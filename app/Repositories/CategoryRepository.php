35cdf5ecf62aed3bdf5c7e6991f1e3cd849df879
ABDALLAH-ATGUIRI <wahbidox@gmail.com>
2023-11-19 15:39:48 +0100

    create repository category with the methods

---
+++ app/Repositories/CategoryRepository.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\Category;
+
+class CategoryRepository implements RepositoryInterface
+{
+    protected $model;
+
+    public function __construct(Category $category)
+    {
+        $this->model = $category;
+    }
+
+    public function getAll()
+    {
+        return $this->model->all();
+    }
+
+    public function find($id)
+    {
+        return $this->model->find($id);
+    }
+
+    public function create(array $data)
+    {
+        return $this->model->create($data);
+    }
+
+    public function update($id, array $data)
+    {
+        $category = $this->model->find($id);
+
+        if ($category) {
+            $category->update($data);
+            return $category;
+        }
+
+        return null;
+    }
+
+    public function delete($id)
+    {
+        $category = $this->model->find($id);
+
+        if ($category) {
+            $category->delete();
+            return true;
+        }
+
+        return false;
+    }
+}
