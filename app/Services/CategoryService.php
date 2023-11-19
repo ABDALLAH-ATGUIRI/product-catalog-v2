7eb800ecaa20908ab4c01cb900120fee3965b0cd
ABDALLAH-ATGUIRI <wahbidox@gmail.com>
2023-11-19 16:05:28 +0100

    create category service

---
+++ app/Services/CategoryService.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Services;
+
+use App\Repositories\CategoryRepository;
+
+class CategoryService
+{
+    protected $categoryRepository;
+
+    public function __construct(CategoryRepository $categoryRepository)
+    {
+        $this->categoryRepository = $categoryRepository;
+    }
+
+    public function createCategory($name, $parentId)
+    {
+        return $this->categoryRepository->create(['name' => $name, 'parent_id' => $parentId]);
+    }
+
+    public function deleteCategory($id)
+    {
+        return $this->categoryRepository->delete($id);
+    }
+}
