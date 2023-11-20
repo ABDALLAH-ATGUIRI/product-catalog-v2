77258847c4a097656e0a5ee6fb8d0ed9ccb03934
ABDALLAH-ATGUIRI <wahbidox@gmail.com>
2023-11-20 01:03:24 +0100

    create service product

---
+++ app/Services/ProductService.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Services;
+
+use App\Repositories\CategoryRepository;
+use App\Repositories\ProductRepository;
+
+class ProductService
+{
+    protected $productRepository;
+
+    public function __construct(ProductRepository $productRepository)
+    {
+        $this->productRepository = $productRepository;
+    }
+
+    public function createProduct($name, $description, $price, $image, $categoryIds)
+    {
+        $data = [
+            'name' => $name,
+            'description' => $description,
+            'price' => $price,
+            'image' => $image,
+        ];
+
+        $product = $this->productRepository->create($data);
+        $product->categories()->attach(str_split($categoryIds[0]));
+
+        return $product;
+    }
+}
