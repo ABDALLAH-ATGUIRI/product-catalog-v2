8f8375b9f96be05494039039a6a1d78f81668898
ABDALLAH-ATGUIRI <wahbidox@gmail.com>
2023-11-21 14:44:30 +0100

    create search request

---
+++ app/Http/Requests/SearchRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class SearchRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'product_name' => 'nullable|string|max:255',
+            'price_min' => 'nullable|numeric',
+            'price_max' => 'nullable|numeric',
+            'category' => 'nullable|numeric|exists:categories,id',
+            'current_page' => 'nullable|numeric',
+        ];
+    }
+}
